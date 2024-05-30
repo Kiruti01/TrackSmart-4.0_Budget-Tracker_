22080876a03c4f1620de7f7fcdd8fe764239f44d
Michael Kiruti <mikekirutic@gmail.com>
2024-05-30 10:08:09 +0300

    Update Logo.tsx

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -1,10 +1,10 @@
-import { PiggyBank } from 'lucide-react'
+import { Landmark } from 'lucide-react'
 import React from 'react'
 
 function Logo() {
     return (
         <a href='/' className='flex items-center gap-2'>
-            <PiggyBank className='stroke h-11 w-11 stroke-amber-500 stroke-[1.5]' />
+            <Landmark className='stroke h-11 w-11 stroke-amber-500 stroke-[1.5]' />
             <p className="bg-gradient-to-r from-amber-400 to-orange-500 bg-clip-text text-3xl font-bold leading-tight tracking-tighter text-transparent">TrackSmart 4</p>
         </a>
     )
