c8275c7d5c4c47fa04405729123f94e87db1e856
Michael Kiruti <mikekirutic@gmail.com>
2024-05-30 10:12:45 +0300

    Update Logo name.tsx

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -5,7 +5,7 @@ function Logo() {
     return (
         <a href='/' className='flex items-center gap-2'>
             <Landmark className='stroke h-11 w-11 stroke-amber-500 stroke-[1.5]' />
-            <p className="bg-gradient-to-r from-amber-400 to-orange-500 bg-clip-text text-3xl font-bold leading-tight tracking-tighter text-transparent">TrackSmart 4</p>
+            <p className="bg-gradient-to-r from-amber-400 to-orange-500 bg-clip-text text-3xl font-bold leading-tight tracking-tighter text-transparent">TrackSmart</p>
         </a>
     )
 }
@@ -14,7 +14,7 @@ export function LogoMobile() {
     return (
         <a href="/" className="flex items-center gap-2">
             <p className="bg-gradient-to-r from-amber-400 to-orange-500 bg-clip-text text-3xl font-bold leading-tight tracking-tighter text-transparent">
-                TrackSmart 4
+                TrackSmart
             </p>
         </a>
     );
